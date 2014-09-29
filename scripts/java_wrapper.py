2478d4a604e972cae4bcc2cd722ca4f7feec5cfd
Georgios Koloventzos <gkoloven@cs.columbia.edu>
2014-09-29 21:11:43 +0000

    running of the command

--- scripts/java_wrapper.py
+++ scripts/java_wrapper.py
@@ -7,33 +7,32 @@ from os.path import expanduser
 import shlex, subprocess
 
 parser = argparse.ArgumentParser()
-parser.parse_args()
 parser.add_argument("user", help="The user's google id")
 parser.add_argument("-s","--search", help="A string to searched in the results")
+args=parser.parse_args()
 
 home = expanduser("~")
-user_path = home + os.sep + "Users" + os.sep + parser.user
+user_path = home + os.sep + "Users" + os.sep + args.user
 candidates = user_path + os.sep + "candidates"
 success = user_path + os.sep + "success"
 
 if not os.path.isdir(candidates):
-	os.makedir(candidates)
+	os.makedirs(candidates)
 if not os.path.isdir(success):
-        os.makedir(success)
+        os.makedirs(success)
 
 directory = home + os.sep + "BarcodeLocalizer"
 
-if not chdir(directory):
-	sys.exit(-1)
+os.chdir(directory)
 
-cmd = "java -Djava.library.path=\"/usr/local/share/OpenCV/java/\" -cp " + home + "/opencv-2.4.9/build/bin/opencv-249.jar:.:./Barcode.jar:./javase-3.1.0.jar:./core-3.1.0.jar BarcodeBatchTester -dir " + user_path
+cmd = " sudo java -Djava.library.path=\"/usr/local/share/OpenCV/java/\" -cp " + home + "/opencv-2.4.9/build/bin/opencv-249.jar:.:./Barcode.jar:./javase-3.1.0.jar:./core-3.1.0.jar BarcodeBatchTester --matrix -dir " + "../Users" + os.sep + args.user
 
 arguments = shlex.split(cmd)
-process = subprocess.Popen(args)
-i(output, stderr) = process.communicate()
+process = subprocess.Popen(arguments)
+(output, stderr) = process.communicate()
 
-if p.returncode:
+if not process.returncode:
 	print "running"
-else
+else:
 	print "not running"
 
