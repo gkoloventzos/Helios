970e83e5fcd5f082f2daaea65216193497d1a963
Georgios Koloventzos <gkoloven@cs.columbia.edu>
2014-09-29 20:05:13 +0000

    command creation

--- scripts/java_wrapper.py
+++ scripts/java_wrapper.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-
 import argparse
+import sys
+import os
 from os.path import expanduser
-
-
+import shlex, subprocess
 
 parser = argparse.ArgumentParser()
 parser.parse_args()
@@ -20,4 +20,20 @@ if not os.path.isdir(candidates):
 	os.makedir(candidates)
 if not os.path.isdir(success):
         os.makedir(success)
+
+directory = home + os.sep + "BarcodeLocalizer"
+
+if not chdir(directory):
+	sys.exit(-1)
+
+cmd = "java -Djava.library.path=\"/usr/local/share/OpenCV/java/\" -cp " + home + "/opencv-2.4.9/build/bin/opencv-249.jar:.:./Barcode.jar:./javase-3.1.0.jar:./core-3.1.0.jar BarcodeBatchTester -dir " + user_path
+
+arguments = shlex.split(cmd)
+process = subprocess.Popen(args)
+i(output, stderr) = process.communicate()
+
+if p.returncode:
+	print "running"
+else
+	print "not running"
 
