edb28d74dbb1851c3e483de3a1f6e87f78ec4301
Georgios Koloventzos <gkoloven@cs.columbia.edu>
2014-09-30 16:48:56 +0000

    adding decode option and search

--- scripts/java_wrapper.py
+++ scripts/java_wrapper.py
@@ -9,6 +9,8 @@ import shlex, subprocess
 parser = argparse.ArgumentParser()
 parser.add_argument("user", help="The user's google id")
 parser.add_argument("-s","--search", help="A string to searched in the results")
+parser.add_argument("-d","--decode", help="To decode again", action="store_true")
+
 args=parser.parse_args()
 
 home = expanduser("~")
@@ -25,14 +27,25 @@ directory = home + os.sep + "BarcodeLocalizer"
 
 os.chdir(directory)
 
-cmd = " sudo java -Djava.library.path=\"/usr/local/share/OpenCV/java/\" -cp " + home + "/opencv-2.4.9/build/bin/opencv-249.jar:.:./Barcode.jar:./javase-3.1.0.jar:./core-3.1.0.jar BarcodeBatchTester --matrix -dir " + "../Users" + os.sep + args.user
+if args.decode:
+	cmd = "sudo java -Djava.library.path=\"/usr/local/share/OpenCV/java/\" -cp " + home\
+		+ "/opencv-2.4.9/build/bin/opencv-249.jar:.:./Barcode.jar:./javase-3.1.0.jar:./core-3.1.0.jar BarcodeBatchTester --matrix -dir ../Users"\
+		+ os.sep + args.user
+	arguments = shlex.split(cmd)
+	process = subprocess.Popen(arguments)
+	(output, stderr) = process.communicate()
 
-arguments = shlex.split(cmd)
-process = subprocess.Popen(arguments)
-(output, stderr) = process.communicate()
+	if not process.returncode:
+		print "running"
+	else:
+		print "not running"
+		sys.exit(0)
 
-if not process.returncode:
-	print "running"
-else:
-	print "not running"
+if not args.search:
+	sys.exit(0)
+
+search_cmd = "grep \"" + args.search + "\" " + user_path + os.sep + "results.txt"
+search_arguments = shlex.split(search_cmd)
+search_process = subprocess.Popen(search_arguments)
+(s_output, s_stderr) = search_process.communicate()
 
